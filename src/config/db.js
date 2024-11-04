c1328f34803e97a2dba2e87c41f13e73b8cb2cc1
Parth-Thoriya <thoriyaparth1011@gmail.com>
2024-11-04 09:18:31 +0530

    udpated mondb-atlas-config.txt

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,9 @@ const mongoURL = process.env.db;
 const connectDB = () => {
   return mongoose
     .connect(mongoURL)
-    .then(() => console.log("MongoDB connected successfully..."))
+    .then(() => console.log("MongoDB connected successfully...")).catch((e)=>{
+      console.log(e,mongoURL)
+    })
 };
 
 module.exports = { connectDB };
